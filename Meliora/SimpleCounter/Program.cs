62f9827dd05304c3b26dc46534f061af72c71582
Martin Petrella <martinpetrella62@gmail.com>
2024-03-07 13:54:44 -0500

    Made SimpleCounter and SimpleCounterUI projects. Updated project README.md

---
+++ Meliora/SimpleCounter/Program.cs
@@ -0,0 +1,51 @@
+﻿var count = 1;
+
+// Default parameters
+var nursingFactorDefault = 3;
+var melioraFactorDefault = 7;
+var maxCountDefault = 50;
+var intervalDefault = 1;
+
+// Working parameters
+var nursingFactor = nursingFactorDefault;
+var melioraFactor = melioraFactorDefault;
+var maxCount = maxCountDefault;
+var interval = intervalDefault;
+
+// Set working parameters from command line
+// For the sake of simplicity and code challenge deadline
+// I'm not going to validate command line arguments
+// but will simply assume the values are reasonable
+// positive integers
+if (Environment.GetCommandLineArgs().Length == 5)
+{
+    nursingFactor = int.Parse(Environment.GetCommandLineArgs()[1]);
+    melioraFactor = int.Parse(Environment.GetCommandLineArgs()[2]);
+    maxCount = int.Parse(Environment.GetCommandLineArgs()[3]);
+    interval = int.Parse(Environment.GetCommandLineArgs()[4]);
+}
+
+// Count until max is reached
+while (count <= maxCount)
+{
+    // Set output message to current count
+    var message = string.Concat(count.ToString(), " ");
+
+    // Append Nursing to message when count is a multiple of nursingFactor
+    if ((count % nursingFactor) == 0)
+    {
+        message = string.Concat(message, "Nursing ");
+    }
+
+    // Append Meliora to message when count is a multiple of melioraFactor
+    if ((count % melioraFactor) == 0)
+    {
+        message = string.Concat(message, "Meliora");
+    }
+
+    Console.WriteLine(message);
+
+    count = count + 1;
+
+    Thread.Sleep(interval * 1000);
+}
---
+++ Meliora/SimpleCounterUI/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SimpleCounterUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.CountTextBox.Text = "50";
+            this.NursingFactorTextBox.Text = "3";
+            this.MelioraFactorTextBox.Text = "7";
+            this.CountRateTextBox.Text = "1";
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    this.StartButton.IsEnabled = false;
+                    this.CountTextBox.IsEnabled = false;
+                    this.NursingFactorTextBox.IsEnabled = false;
+                    this.MelioraFactorTextBox.IsEnabled = false;
+                    this.CountRateTextBox.IsEnabled = false;
+                }));
+
+                string? message;
+                Process cmd = new Process();
+                cmd.StartInfo.FileName = "SimpleCounter.exe";
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false;
+
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    cmd.StartInfo.Arguments = string.Format("{0} {1} {2} {3}",
+                                              this.NursingFactorTextBox.Text,
+                                              this.MelioraFactorTextBox.Text,
+                                              this.CountTextBox.Text,
+                                              this.CountRateTextBox.Text);
+                }));
+
+                cmd.Start();
+
+                do
+                {
+                    message = cmd.StandardOutput.ReadLine();
+
+                    Dispatcher.Invoke(new Action(() =>
+                    {
+                        this.MessageLabel.Content = message == null ? "---" : message;
+                    }));
+                } while (message != null);
+
+                cmd.WaitForExit();
+
+                Dispatcher.Invoke(new Action(() =>
+                {
+                    this.StartButton.IsEnabled = true;
+                    this.CountTextBox.IsEnabled = true;
+                    this.NursingFactorTextBox.IsEnabled = true;
+                    this.MelioraFactorTextBox.IsEnabled = true;
+                    this.CountRateTextBox.IsEnabled = true;
+                }));
+            });
+        }
+
+        private void FactorTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            this.DescriptionLabel.Content = string.Format("A simple counter displaying a message when count is a multiple of {0} or {1}.",
+                this.NursingFactorTextBox.Text, this.MelioraFactorTextBox.Text);
+        }
+    }
+}
